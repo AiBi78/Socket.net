c28ce8183650ddbc8f139a824642f615eeb8788e
Ali Behrozi <AiBi78@github.com>
2020-02-10 01:46:41 +0330

    test class

---
+++ app/src/main/java/ir/indata/delivery/net/test.java
@@ -0,0 +1,12 @@
+package ir.indata.delivery.net;
+
+import ir.indata.delivery.Pusher;
+
+public class test extends Pusher {
+
+
+    @Override
+    protected void doInBackground(String... params) {
+
+    }
+}
